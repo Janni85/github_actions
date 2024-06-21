bfd271b1231bf868f889eadf4bf9459990ad052f
Janni85 <papajanni@gmx.de>
2024-06-21 17:57:30 +0200

    terraform init und .tf dateien erstellt

--- infra/s3_hosting.tf
+++ infra/s3_hosting.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "todo-app" {
-  bucket        = var.bucket_name
+  bucket = var.bucket_name
   force_destroy = true
 
   tags = {
@@ -27,15 +27,42 @@ resource "aws_s3_bucket_website_configuration" "todo-app-web-config" {
   error_document {
     key = "error.html"
   }
+
+}
+
+# resource "aws_s3_bucket_policy" "allow_access" {
+#   bucket = aws_s3_bucket.todo-app.id
+#   policy = <<POLICY
+# {
+#   "Id": "Policy1718964006332",
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Sid": "Stmt1718964005386",
+#       "Action": [
+#         "s3:GetObject"
+#       ],
+#       "Effect": "Allow",
+#       "Resource": "arn:aws:s3:::${aws_s3_bucket.todo-app.bucket}/*",
+#       "Principal": "*"
+#     }
+#   ]
+# }
+# POLICY
+# }
+
+resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
+  bucket = aws_s3_bucket.todo-app.id
+  policy = data.aws_iam_policy_document.allow_access_from_another_account.json
 }
 
 data "aws_iam_policy_document" "allow_access_from_another_account" {
-  statement {
-    sid    = "1"
+    statement {
+    sid = "1"
     effect = "Allow"
 
     principals {
-      type        = "AWS"
+      type = "AWS"
       identifiers = ["*"]
     }
 
@@ -43,8 +70,6 @@ data "aws_iam_policy_document" "allow_access_from_another_account" {
       "s3:GetObject",
     ]
 
-    resources = [
-      "${aws_s3_bucket.todo-app.arn}/*",
-    ]
+    resources = ["${aws_s3_bucket.todo-app.arn}/*",]
   }
 }
